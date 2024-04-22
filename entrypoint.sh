4b0638d2fc7b2e29f7fb6269f62c3bc192bd8c2a
Anthony Beato Abreu <axba0001@ce.pucmm.edu.do>
2024-04-22 16:28:56 -0400

    cambio en entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ set -e
 source /opt/ros/iron/setup.bash
 
 # Ir al directorio de trabajo y construir el proyecto
-cd /home/${USERNAME}/simulation_ws
-colcon build --symlink-install
-source install/setup.bash
+# cd /home/ubuntu/simulation_ws
+# colcon build --symlink-install
+# source install/setup.bash
 
 # (Aquí irían comandos adicionales para lanzar tu simulación)
 gazebo &
