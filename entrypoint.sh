d15ebb7f34d3ab6b386d7c26b253e7e8bc406b90
Anthony Beato Abreu <axba0001@ce.pucmm.edu.do>
2024-04-22 22:51:14 -0400

    intento de arreglo

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,21 +4,22 @@ set -e
 # Configurar ROS
 source /opt/ros/iron/setup.bash
 
-# Ir al directorio de trabajo y construir el proyecto
-# cd /home/ubuntu/simulation_ws
-colcon build --symlink-install
-# source install/setup.bash
-
+# Configuraciones de entorno necesarias para QT y otras aplicaciones
 export DISPLAY=:19
-# (Aquí irían comandos adicionales para lanzar tu simulación)
-vglrun gazebo &
-vglrun rviz2 &
-
 export QT_QPA_PLATFORM=offscreen
 export AUDIODEV=null
 export ALSA_CONFIG_PATH=/dev/null
 export XDG_RUNTIME_DIR=/tmp
 export QT_DEBUG_PLUGINS=1
+
+# Ir al directorio de trabajo y construir el proyecto
+# cd /home/ubuntu/simulation_ws
+colcon build --symlink-install
+# source install/setup.bash
+
+# Iniciar aplicaciones con VirtualGL
+vglrun gazebo &
+vglrun rviz2 &
 
 # Mantener el contenedor en ejecución si no se pasan comandos adicionales
 exec "$@"
